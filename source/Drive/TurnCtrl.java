f9fe2269a2c25f2b011b8bb8cf1254cd7462c918
yusan <s1000614@sendai-nct.jp>
2015-08-17 13:19:22 +0900

    Renew class TurnCtrl  Add constructor  Renew mathod MotorCtrl

---
+++ source/Drive/TurnCtrl.java
@@ -0,0 +1,29 @@
+
+
+package Drive;
+
+public class TurnCtrl {
+
+	private TurnOperater turnOperater;
+
+	private Brightness brightness;
+
+	private PCtrl pCtrl;
+
+	TurnCtrl(){
+		turnOperater = new TurnOperater();
+		brightness = new Brightness();
+		pCtrl = new PCtrl();
+	}
+
+
+	//モータを制御する
+	public void MotorCtrl() {
+		float bright = brightness.getBrightness();
+		float turn = pCtrl.calcBrightnessCtrl(bright);
+		turnOperater.setOperate(turn);
+		turnOperater.MotorCtrl();
+
+	}
+
+}
