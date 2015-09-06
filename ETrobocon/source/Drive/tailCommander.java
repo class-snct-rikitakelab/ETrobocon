aebab43b8d6ab582bfb613df37c13e0248cdbcf4
yusan <s1000614@sendai-nct.jp>
2015-09-06 14:19:20 +0900

    Renew class tailCommander  Add comments

--- ETrobocon/source/Drive/tailCommander.java
+++ ETrobocon/source/Drive/tailCommander.java
@@ -11,12 +11,14 @@ public class tailCommander {
 		tailctrl = new tailCtrl();
 	}
 
+	//テイルを接地する動作を定義
 	public void standTail(){
 		tailctrl.setTargetValue(ANGLE_STAND);
 		float pwm = tailctrl.calcTailCtrl();
 		tailctrl.operateTail(pwm);
 	}
 
+	//テイルを地面から離す動作を定義
 	public void sitTail(){
 		tailctrl.setTargetValue(ANGLE_SIT);
 		float pwm = tailctrl.calcTailCtrl();
