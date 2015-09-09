34a56af92a18f0d086e8fd3bc59b645047f6d06c
AbeYusaku <s1100603@sendai-nct.jp>
2015-09-09 13:27:54 +0900

    calibration fix

--- ETrobocon/source/Drive/StopRun.java
+++ ETrobocon/source/Drive/StopRun.java
@@ -45,7 +45,7 @@ public class StopRun {
 		LCD.drawString("Curve:" + curve, 2, 0);
 		LCD.drawString("CurveC:" + curveCount, 2, 1);
 
-		if(curveCount>=0){//カーブ回数が少ないとこっちの処理に入らないため常にfalseを返す
+		if(curveCount>=2){//カーブ回数が少ないとこっちの処理に入らないため常にfalseを返す
 			// 距離を測る処理 **************************************************************************
 			float distance = cDistance.getDistance();
 			LCD.drawString("Dist: " + distance, 2, 2);
@@ -70,14 +70,12 @@ public class StopRun {
 	}
 
 	public void reset(){
-		grayCount = 0;// 灰色の部分を何回通ったか 二回通ったら止まる信号を出す
-		isGray = false;//灰色の部分であるときに最初の一度だけカウント増加する用
+		grayCount = 0;
+		isGray = false;
 		curveCount = 0;
 		isCurve = false;
 		distanceFromCurve = 0;
 		cGray.setGrayPoint(0);
-		float distance = cDistance.getDistance();
-		if(distance>100)LCD.drawString("Last: " + distance, 2, 6);
 
 	}
 
